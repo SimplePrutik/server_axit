b35b70e803296cf4ce8f14fb7af512ea34d4cd60
SimplePrutik <dimodaktil@yandex.ru>
2017-05-16 14:10:15 +0300

    post configured

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,21 +1,32 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
-  app.get('/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    db.collection('notes').findOne(details, (err, item) => {
+  // app.get('/reviews/:id', (req, res) => {
+  //   const id = req.params.id;
+  //   const details = { '_id': new ObjectID(id) };
+  //   db.collection('reviews').findOne(details, (err, item) => {
+  //     if (err) {
+  //       res.send({'error':'An error has occurred'});
+  //     } else {
+  //       res.send(item);
+  //     } 
+  //   });
+  // });
+
+  app.get('/reviews', (req, res) => {
+    db.collection('reviews').find({}).toArray(function (err, items) {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
-        res.send(item);
+        res.json(items);
       } 
-    });
+  	});
   });
 
-  app.post('/notes', (req, res) => {
-    const note = { text: req.body.body, title: req.body.title };
-    db.collection('notes').insert(note, (err, result) => {
+  app.post('/reviews', (req, res) => {
+    console.log(req.body)
+    const note = { review: req.body.review, author: req.body.author, job: req.body.job };
+    db.collection('reviews').insert(note, (err, result) => {
       if (err) { 
         res.send({ 'error': 'An error has occurred' }); 
       } else {
@@ -24,11 +35,11 @@ module.exports = function(app, db) {
     });
   });
 
-  app.put('/notes/:id', (req, res) => {
+  app.put('/reviews/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
-    const note = { text: req.body.body, title: req.body.title };
-    db.collection('notes').update(details, note, (err, result) => {
+    const note = { review: req.body.review, author: req.body.author, job: req.body.job };
+    db.collection('reviews').update(details, note, (err, result) => {
       if (err) {
           res.send({'error':'An error has occurred'});
       } else {
@@ -37,10 +48,10 @@ module.exports = function(app, db) {
     });
   });
 
-  app.delete('/notes/:id', (req, res) => {
+  app.delete('/reviews/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
-    db.collection('notes').remove(details, (err, item) => {
+    db.collection('reviews').remove(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
